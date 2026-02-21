80e3780803bab27f6b85b648d778fcd3ef2f2f50
Ibrahim Abdulaziz <ibrahimabdullaziz55@gmail.com>
2026-02-21 14:21:32 +0200

    finishing section 3

--- section 3/code/app.js
+++ section 3/code/app.js
@@ -1,46 +1,7 @@
 const http = require("http");
-const fs = require("fs");
+const router = require("./router");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<header><title>title</title></header>");
-    res.write(
-      `<body> <form action='/message' method='POST'> <input type='text' name='message' /> <button>send a message</button>  </form>  </body`,
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        if (!err) {
-          res.statusCode = 302;
-          res.setHeader("Location", "/");
-          return res.end();
-        }
-      });
-    });
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<header><title>title</title></header><body>");
-  // res.write(fs.readFileSync("./message", "utf-8"));
-  res.write("</body></html>");
-  res.end();
-});
+console.log(router.someDummyText);
+const server = http.createServer(router.handler);
 
 server.listen(3000);
