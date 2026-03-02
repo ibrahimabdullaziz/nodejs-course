8c7bb0e5332f434dddb066492a597b6aeea9a9a1
Ibrahim Abdulaziz <ibrahimabdullaziz55@gmail.com>
2026-03-02 11:35:43 +0200

    setup sequelize and start the project

---
+++ section 11 (sqelueize)/code/util/database.js
@@ -0,0 +1,23 @@
+// const mysql = require("mysql2");
+
+// require("dotenv").config();
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  { host: process.env.DB_HOST, dialect: "mysql" },
+);
+
+module.exports = sequelize;
