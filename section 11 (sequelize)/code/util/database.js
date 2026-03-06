ddd20497c368aaf589f8bb2733a877eb0216342e
Ibrahim Abdulaziz <ibrahimabdullaziz55@gmail.com>
2026-03-06 11:51:02 +0200

    finshing section 11

--- section 11 (sequelize)/code/util/database.js
+++ section 11 (sequelize)/code/util/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("node-complete", "root", "ibrahim26124", {
-  dialect: "mysql",
-  host: "localhost",
-});
+require("dotenv").config();
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+  },
+);
 
 module.exports = sequelize;
