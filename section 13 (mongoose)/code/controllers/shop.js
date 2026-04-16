a80b0aae6008491832212232c5024e0c1a12a8a3
Ibrahim Abdulaziz <ibrahimabdullaziz55@gmail.com>
2026-04-16 02:43:57 +0200

    Refactor order handling to use Mongoose,

--- section 13 (mongoose)/code/controllers/shop.js
+++ section 13 (mongoose)/code/controllers/shop.js
@@ -1,5 +1,6 @@
 const Product = require("../models/product");
 const User = require("../models/user");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -89,15 +90,32 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
   req.user
-    .addOrder()
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user,
+        },
+        products: products,
+      });
+      return order.save();
+    })
     .then((result) => {
+      req.user.cart = { items: [] };
+      return req.user.save();
+    })
+    .then(() => {
       res.redirect("/orders");
     })
     .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  User.getOrders(req.user._id)
+  Order.find({ "user.userId": req.user._id })
     .then((orders) => {
       res.render("shop/orders", {
         path: "/orders",
