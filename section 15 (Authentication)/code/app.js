f2b21211a56f16ef2150749602dee40b1efe685f
Ibrahim Abdulaziz <ibrahimabdullaziz55@gmail.com>
2026-04-20 00:16:24 +0200

    feat: implement user authentication with route protection and csrf attacks

--- section 15 (Authentication)/code/app.js
+++ section 15 (Authentication)/code/app.js
@@ -7,11 +7,13 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
 const MONGODB_URI = process.env.MONGODB_URI;
+const csrfProtection = csrf();
 
 const app = express();
 const store = new MongoDBStore({
@@ -51,6 +53,13 @@ app.use((req, res, next) => {
     })
     .catch((err) => console.log(err));
 });
+app.use(csrfProtection);
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -68,18 +77,6 @@ mongoose
   .connect(MONGODB_URI)
   .then((result) => {
     return User.findOne();
-  })
-  .then((user) => {
-    if (!user) {
-      const user = new User({
-        name: "ibrahim",
-        email: "ibrahim@test.com",
-        cart: {
-          items: [],
-        },
-      });
-      return user.save();
-    }
   })
   .then(() => {
     app.listen(PORT, () => {
--- section 15 (Authentication)/code/controllers/auth.js
+++ section 15 (Authentication)/code/controllers/auth.js
@@ -1,39 +1,81 @@
-const User = require('../models/user');
+const User = require("../models/user");
+const crypt = require("bcryptjs");
 
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: false
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
   });
 };
 
 exports.getSignup = (req, res, next) => {
-  res.render('auth/signup', {
-    path: '/signup',
-    pageTitle: 'Signup',
-    isAuthenticated: false
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
+  const { email, password } = req.body;
+
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        return res.redirect("/login");
+      }
+      return crypt.compare(password, user.password).then((doMatch) => {
+        if (doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          req.session.save((err) => {
+            console.log(err);
+            res.redirect("/");
+          });
+        } else {
+          res.redirect("/login");
+        }
       });
     })
-    .catch(err => console.log(err));
+    .catch((err) => {
+      console.log("ERROR!.....", err);
+      return res.redirect("/login");
+    });
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+  const { email, password, confirmedPassword } = req.body;
+
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect("/signup");
+      }
+      return crypt
+        .hash(password, 12)
+        .then((cryptedPassword) => {
+          const user = new User({
+            email: email,
+            password: cryptedPassword,
+            cart: { items: [] },
+          });
+
+          return user.save();
+        })
+        .then((result) => {
+          console.log("USER CREATED!");
+          res.redirect("/login");
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     console.log(err);
-    res.redirect('/');
+    res.redirect("/");
   });
 };
